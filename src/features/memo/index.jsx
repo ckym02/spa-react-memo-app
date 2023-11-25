8902330443ef2db5e2a1856195c43628058d800a
koyama-c <koyama@linkedge.jp>
2023-11-25 21:01:01 +0900

    コンポーネントのファイル名を大文字にする

--- src/features/memo/index.jsx
+++ src/features/memo/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { MemoForm } from './components/memoForm';
+import { MemoForm } from './components/MemoForm';
 import {
   initMemo,
   getAllMemos,
